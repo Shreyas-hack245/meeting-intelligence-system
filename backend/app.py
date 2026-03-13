f7f4cac7913b60725e611bd1499e484158648be2
Shreyas-hack245 <19776shreyasbhat@gmail.com>
2026-03-13 19:34:31 +0530

    frontend modified

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from routes.transcript_routes import router as transcript_router
 from routes.audio_routes import router as audio_router
 from routes.trello_routes import router as trello_router
 
-app = FastAPI(title="AI Meeting Intelligence System")
+app = FastAPI()
 
 app.include_router(transcript_router)
 app.include_router(audio_router)
@@ -12,4 +12,4 @@ app.include_router(trello_router)
 
 @app.get("/")
 def home():
-    return {"message": "Backend Running Successfully"}
+    return {"message": "Backend Running"}
